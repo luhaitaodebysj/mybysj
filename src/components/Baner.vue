72aed9410a77b2802bb2cbe54bc78025aeb16b58
卢海涛 <1006453754@qq.com>
2017-04-25 19:17:14 +0800

    搜索框

--- src/components/Baner.vue
+++ src/components/Baner.vue
@@ -1,7 +1,8 @@
 <template>
 <div class="baner">
 	<mt-swipe :auto="4000"
-	          :prevent="true">
+	          :prevent="true"
+	          :show-indicators="false">
 		<mt-swipe-item><img src="../assets/imags/1.jpg" height="240" width="427"></mt-swipe-item>
 		<mt-swipe-item><img src="../assets/imags/2.jpg" height="240" width="427"></mt-swipe-item></mt-swipe-item>
 		<mt-swipe-item><img src="../assets/imags/3.jpg" height="240" width="427"></mt-swipe-item></mt-swipe-item>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,21 +1,57 @@
 <template>
   <div class="search">
-    <mt-search
+<!--     <mt-search
     cancel-text="取消"
     placeholder="搜索"
-    v-bind:class="SearchBackground">
-  </mt-search >
+    v-bind:class="SearchBackground"
+    v-model="value">
+  </mt-search > -->
+  <mt-search autofocus v-model="value" :result="filterResult"></mt-search>
+
+  <group>   
+     <cell title="title" value="value"></cell>
+  </group>
   </div>
 </template>
 
 <script>
+import { Group , cell } from 'vux'
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      SearchBackground:'search-background'
+      SearchBackground:'search-background',
+      searchListTop:'searchListTop',
+      value: '',
+      defaultResult:[
+        'Apple',
+        'Banana',
+        'Orange',
+        'Durian',
+        'Lemon',
+        'Peach',
+        'Cherry',
+        'Berry',
+        'Core',
+        'Fig',
+        'Haw',
+        'Melon',
+        'Plum',
+        'Pear',
+        'Peanut',
+        'Other'
+      ]
     }
+  },
+  computed: {
+    filterResult() {
+      return this.defaultResult.filter(value => new RegExp(this.value, 'i').test(value));
+    }
+  },
+  components:{
+    Group,
+    Cell
   }
 }
 </script>
@@ -26,6 +62,9 @@ export default {
   .search{
     height: 1rem*(50/20);
   }
+  .search>div{
+    text-align: left;
+  }
   //mint-ui样式改变  类替换
   //1.搜索框背景
   .mint-searchbar{
@@ -34,4 +73,58 @@ export default {
   .mint-searchbar-cancel{
     color: @colorddd;
   }
+  .mint-search-list{
+    padding-top: 2.5rem;
+    z-index: 9999;
+  }
 </style>
+<!-- 
+
+
+
+
+
+
+
+<template>
+  <div class="page-search">
+    <mt-search autofocus v-model="value" :result="filterResult"></mt-search>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-search',
+  data() {
+    return {
+      value: '',
+      defaultResult: [
+        'Apple',
+        'Banana',
+        'Orange',
+        'Durian',
+        'Lemon',
+        'Peach',
+        'Cherry',
+        'Berry',
+        'Core',
+        'Fig',
+        'Haw',
+        'Melon',
+        'Plum',
+        'Pear',
+        'Peanut',
+        'Other'
+      ]
+    };
+  },
+  computed: {
+    filterResult() {
+      return this.defaultResult.filter(value => new RegExp(this.value, 'i').test(value));
+    }
+  }
+};
+</script>
+
+<style lang="css">
+</style> -->
