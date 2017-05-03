1c455885d2e6cf8703f06d11e0a51e809db0af32
chrisechen <penggoodlu521@163.com>
2017-05-03 22:15:31 +0800

    新增登录页面，我的详情页面

--- src/page/login/login.vue
+++ src/page/login/login.vue
@@ -4,16 +4,22 @@
          <a href="#"><</a>
       	 {{title}}
       </div>
+     
+     <div class="login-main">
+     <div style="height:2rem;"></div>
+     <div class="logo"></div>
       <form action="" method="post" id="login-post">
       	<div class="username">
       		<label></label>
-      		<input type="tetx" placeholder="账号" name="username">
+      		<input type="text" placeholder="请输入账号" name="username">
       	</div>
       	<div class="password">
       		<label></label>
-      		<input type="tetx" placeholder="密码" name="password">
+      		<input type="text" placeholder="密码" name="password">
       	</div>
+        <input class="loginBtn" type="button" value="登录"/>
       </form>
+      </div>
   </div>
 </template>
 
@@ -26,34 +32,91 @@ export default {
       title:'登录'
     }
   }
+
+
 }
 </script>
 
 <style  scoped lang="less">
+input:focus { outline: none; }
+input{
+  width:14rem;
+  height:2rem;
+  border:none;
+  background:none;
+  line-height:2rem;
+  font-size:17px;
+}
+.login-main{
+  background:white;
+  width:100%;
+  height:100%;
+}
 #login-post{
-	margin-top:5rem;
 	>div{
-		margin: 1rem;
-		height: 2.5rem;
+		height: 2rem;
 	}
 	input{
 		height: 100%;
 	}
 }
 .login-header{
-	height: 2.5rem;
+  height:1.5rem;
 	font-size: 20px;
-	color:white;
+	color:#999999;
 	line-height: 1.5rem;
 	text-align: center;
-	background: #ff6600;
+	background: #F7F7FA;
 	padding: 0.5rem;
 	position: relative;
 	letter-spacing: 3px;
+  border-bottom:1px solid #DCDBE0;
 	>a{
 		position: absolute;
 		left:0.5rem;
-		color: white;
+		color: #999999;
 	}
+}
+
+.username{
+ width:15rem;
+ height:2rem;
+ border-radius:5px;
+ margin:0 auto;
+ background:#DADADA;
+ border:1px solid #B2B2B2;
+ margin-top:4rem;
+ filter:alpha(opacity=70);  
+ -moz-opacity:0.7;  
+ -khtml-opacity: 0.7;  
+  opacity: 0.7;
+}
+.password{
+ width:15rem;
+ height:2rem;
+ border-radius:5px;
+ margin:0 auto;
+ background:#DADADA;
+ border:1px solid #B2B2B2;
+ margin-top:2rem;
+ filter:alpha(opacity=70);  
+ -moz-opacity:0.7;  
+ -khtml-opacity: 0.7;  
+  opacity: 0.7;
+}
+.logo{
+  background:url(../../../static/imags/logo.png) no-repeat;
+  width:195px;
+  height:60px;
+  margin:0 auto;
+}
+.loginBtn{
+  background:#0BD28A;
+  width:15rem;
+  height:2rem;
+  border-radius:5px;
+  font-size:17px;
+  margin-top:2rem;
+  color:white;
 }
 </style>
