925e2c5075eba73f1c2596a56dadac3106deb444
卢海涛 <1006453754@qq.com>
2017-05-03 22:20:47 +0800

    修改购物车页面

--- src/page/shop/shop.vue
+++ src/page/shop/shop.vue
@@ -1,33 +1,103 @@
 <template>
 	<div id="shop">
 	    <div class="shopTitle">购物车</div>
-	    <div class="shopList">
-	    	<div class="shopListBox">
-	    	  <div class="shoppingImg">
-	    	  	<img src="">
-	    	  </div>
-      	      <div class="shoppingDetail">
-      	      	 <ul>
-      	      	 	<li class="shoppingDetailName">自行车</li>
-      	      	 	<li></li>
-      	      	 	<li></li>
-      	      	 	<li></li>
-      	      	 </ul>
-      	      </div>		
+	    <div id="shopList">
+	    	<div class="shopList" v-for="item in items">
+	    		<div class="shopListBox">
+	    			<div class="shoppingImg">
+	    				<img :src="item.imgurl">
+	    			</div>
+	    			<div class="shoppingDetail">
+	    				<ul>
+	    					<li class="shoppingDetailName">{{item.title}}</li>
+	    					<li></li>
+	    					<li>    
+	    						<group>
+	    							<x-number :name="title" :title="title"></x-number>
+	    						</group>
+	    					</li>
+	    					<li class="shoppingDetailNum">
+	    						<span>数量：x{{item.num}}</span>
+	    						<span>￥{{item.money}}</span>
+	    					</li>
+	    				</ul>
+	    			</div>		
+	    		</div>
+	    </div>
+	    </div>
+
+	    <div class="shopSurePay">
+	    	<div class="shopSurePayLeft">
+	    		<p>共<i>16</i>件商品</p>
+	    		<p>总计￥<i>116.0</i>元</p>
+	    	</div>
+	    	<div class="shopSurePayRight">
+	    		进入结算
 	    	</div>
 	    </div>
 	</div>
 </template>
 <script type="text/javascript">
+import {Group , XNumber,XSwitch } from 'vux'
+//初始化页面高度
+function setHeight() {
+	var height=$(window).height()-153;
+	$("#shopList").css('height',height);
+}
 export default {
+  components: {
+  	XNumber,Group,XSwitch
+  },
   name:'shop',
   data () {
     return {
-      selected:"主页"
+      selected:"主页",
+      totalMoney:0,
+      goodsNum:0,
+      title:'数量',
+      items:[{
+      	imgurl:'../../../static/imags/book/java.jpg',
+      	title:'自行车',
+      	num:7,
+      	money:555.00
+      },{
+      	imgurl:'../../../static/imags/book/java.jpg',
+      	title:'自行车',
+      	num:7,
+      	money:555.00
+      },{
+      	imgurl:'../../../static/imags/book/java.jpg',
+      	title:'自行车',
+      	num:7,
+      	money:555.00
+      },{
+      	imgurl:'../../../static/imags/book/java.jpg',
+      	title:'自行车',
+      	num:7,
+      	money:555.00
+      },{
+      	imgurl:'../../../static/imags/book/java.jpg',
+      	title:'自行车',
+      	num:7,
+      	money:555.00
+      },{
+      	imgurl:'../../../static/imags/book/java.jpg',
+      	title:'自行车',
+      	num:7,
+      	money:555.00
+      }]
     }
+  },
+  created:function() {
+  	setHeight();
+  },
+  mounted:function() {
+  	//当元素渲染出来后
+  	setHeight();
+  	 
   }
 }
-	
+ setHeight();
 </script>
 
 <style lang="less" scoped>
@@ -35,11 +105,15 @@ ul,li{
 	list-style-type: none;
 	padding: 0;
 	margin: 0;
-}
+}   
+
 	#shop{
 		width:100%;
 		height:100%;
 		position: relative;
+	}
+	#shopList{
+		overflow: scroll;
 	}
 	.shopTitle{
 		width: 100%;
@@ -65,9 +139,65 @@ ul,li{
 		float: left;
 		width: 6rem;
 		height: 6rem;
+		margin-right: 0.75rem;
+		>img{
+			width: 100%;
+		}
 	}
 	.shoppingDetail{
-		float: left;
+		margin-left: 6.75rem;
+		li{
+			height: 1.5rem;
+			line-height: 1.5rem;
+			color: #575757;
+		}
 	}
+	.shoppingDetailName{
+		text-align: left;
+	}
+	.shoppingDetailNum{
+		span{
+			display: inline-block;
+			height: 100%
+		}
+		span:nth-child(1){
+			float: left;
+		}
+		span:nth-child(2){
+			float: right;
+			font-size: 15px;
+			color: #b7b8b7;
+		}
+	}
+	.shopSurePay{
+		position: fixed;
+		height: 2.5rem;
+		width: 100%;
+		background-color: white;
+		bottom: 53px;
+		padding: 0.5rem;
+	}
+	.shopSurePayLeft{
+		display: inline-block;
+		float: left;
+		p{
+			margin: 0;
+			font-size: 12px;
+			text-align: left;
+			i{
+				color: #ff6600;
+			}
+		}
 
+	}
+	.shopSurePayRight{
+		display: inline-block;
+		float: right;
+		width: 5rem;
+		height: 100%;
+		background-color: black;
+		color:white;
+		border-radius: 4px;
+		line-height: 30px;
+	}
 </style>
